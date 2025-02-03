589f739d4841422028b23e85bdc306aacda2204b
SYED AARIJ <aarijtaqvi@gmail.com>
2025-02-04 02:55:07 +0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,14 +1,5 @@
-<<<<<<< login_page
-import React from "react";
-import Login_page from "./login_page"; // Import Login component
-function App() {
-  return (
-    <div>
-      <Login_page />
-=======
 import React from "react";
 import Cart from "./components/Cart/Cart";
-
 function App() {
   return (
     <div>
