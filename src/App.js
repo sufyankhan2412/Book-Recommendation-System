a1b7314ed23844edd339674925872a2d12eb5836
SYED AARIJ <aarijtaqvi@gmail.com>
2025-02-05 03:38:08 +0500

    Completed work

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
 import React from "react";
 import Cart from "./components/Cart/Cart";
+import SignupPage from "./components/signup/signup_page";
+import home_page from "./home_page"
+
 function App() {
   return (
     <div>
-      <Cart />
+      <home_page/>
     </div>
   );
 }
