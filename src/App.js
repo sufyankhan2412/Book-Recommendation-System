f5a9be632cec92883bbf2c55e26a44799853ee87
Hasnain Abbasi <ha081622@gmail.com>
2025-02-06 17:51:24 +0500

    Resolved conflicts

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import HomePage from "./components/home_page/home_page";
 function App() {
   return (
     <div>
-      <HomePage/> {/* Use the component with an uppercase letter */}
+      <HomePage/> 
     </div>
   );
 }
