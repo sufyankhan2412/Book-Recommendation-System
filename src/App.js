32cf92017021544446130db7718a6f50553fee25
SYED AARIJ <aarijtaqvi@gmail.com>
2025-02-08 01:03:16 +0500

    Completed work

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from "react";
 import 'font-awesome/css/font-awesome.min.css';
-import HomePage from "./components/home_page/home_page";
+//import HomePage from "./components/home_page/home_page";
 //import EventsPage from "./components/Event_page/Event_page";
+import  blogstyle1  from "./components/blogstyle1/style1";
+
 function App() {
   return (
     <div>
-      <HomePage/> 
+      <blogstyle1/> 
     </div>
   );
 }
