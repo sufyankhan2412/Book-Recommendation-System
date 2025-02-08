cce1cbf3aefbf83ef019573723c895299476931b
SYED AARIJ <aarijtaqvi@gmail.com>
2025-02-09 00:26:01 +0500

    Completed work on single post blogstyle2

--- src/App.js
+++ src/App.js
@@ -2,12 +2,13 @@ import React from "react";
 import 'font-awesome/css/font-awesome.min.css';
 // import HomePage from "./components/home_page/home_page";
 // import EventsPage from "./components/Event_page/Event_page";
-import BlogStyle1 from "./components/blogstyle1/style1"; 
+//import BlogStyle1 from "./components/blogstyle1/style1"; 
+import Blogstyle2 from "./components/blogstyle2/blogstyle2";
 
 function App() {
   return (
     <div>
-      <BlogStyle1 /> {/* Use uppercase component name */}
+      <Blogstyle2 /> 
     </div>
   );
 }
