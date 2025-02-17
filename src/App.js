d7961fc14896c376b2ffb3a76c3e326e81c98ea0
sufyankhan2412 <sufyanmk2024801@gmail.com>
2025-02-17 23:52:32 +0500

    updated payment

--- src/App.js
+++ src/App.js
@@ -1,15 +1,18 @@
 
 import React from "react";
-import Cart from "./components/Cart/Cart";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import Payment from "./components/Payment/Payment";
+
 
 function App() {
   return (
-    <div className="App">
-      <h1>Welcome to Our Store</h1>
-      <Cart />
-    </div>
+    <Router>
+      <Routes>
+        <Route path="/" element={<Shop />} />
+        <Route path="/Payment" element={<Payment />} />
+      </Routes>
+    </Router>
   );
 }
 
 export default App;
-
