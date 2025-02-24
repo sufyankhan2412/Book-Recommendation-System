466c87e738f3f405c56a4d84cb219f68d420b26e
Hasnain Abbasi <ha081622@gmail.com>
2025-02-25 00:40:33 +0500

    home page

--- src/App.js
+++ src/App.js
@@ -1,17 +1,18 @@
 import React from "react";
 import Payment from "./components/Payment/Payment";
+import HomePage from "./components/home_page/home_page";
 
 const App = () => {
   // Mock data for products and totalAmount
-  const products = [
-    { name: "Product 1", price: 10, description: "Description of Product 1" },
-    { name: "Product 2", price: 20, description: "Description of Product 2" },
-  ];
-  const totalAmount = 30;
+  //const products = [
+   // { name: "Product 1", price: 10, description: "Description of Product 1" },
+    //{ name: "Product 2", price: 20, description: "Description of Product 2" },
+  //];
+  //const totalAmount = 30;
 
   return (
     <div>
-      <Payment products={products} totalAmount={totalAmount} />
+      <HomePage />
     </div>
   );
 };
